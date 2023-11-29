8b7e4509ced3d4c79ecd23039261d4dabe53771b
marco <marco.crucitti96@gmail.com>
2023-11-29 12:37:43 +0100

    t

---
+++ var/cache/prod/smarty/compile/classic/80/ac/9d/80ac9ddb06fe7b43ffdd2f5cd1185536480d2577_2.module.ps_socialfollowps_socialfollow.tpl.cache.php
@@ -0,0 +1,67 @@
+<?php
+/* Smarty version 4.3.1, created on 2023-11-29 12:43:59
+  from 'module:ps_socialfollowps_socialfollow.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.1',
+  'unifunc' => 'content_656723ff463ed5_46041764',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '80ac9ddb06fe7b43ffdd2f5cd1185536480d2577' => 
+    array (
+      0 => 'module:ps_socialfollowps_socialfollow.tpl',
+      1 => 1701189439,
+      2 => 'module',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_656723ff463ed5_46041764 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, false);
+$_smarty_tpl->compiled->nocache_hash = '646806709656723ff460a53_55545531';
+?>
+
+<?php 
+$_smarty_tpl->inheritance->instanceBlock($_smarty_tpl, 'Block_23229079656723ff461617_73248919', 'block_social');
+?>
+
+<?php }
+/* {block 'block_social'} */
+class Block_23229079656723ff461617_73248919 extends Smarty_Internal_Block
+{
+public $subBlocks = array (
+  'block_social' => 
+  array (
+    0 => 'Block_23229079656723ff461617_73248919',
+  ),
+);
+public function callBlock(Smarty_Internal_Template $_smarty_tpl) {
+?>
+
+  <div class="block-social col-lg-4 col-md-12 col-sm-12">
+    <ul>
+      <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['social_links']->value, 'social_link');
+$_smarty_tpl->tpl_vars['social_link']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['social_link']->value) {
+$_smarty_tpl->tpl_vars['social_link']->do_else = false;
+?>
+        <li class="<?php echo htmlspecialchars((string) $_smarty_tpl->tpl_vars['social_link']->value['class'], ENT_QUOTES, 'UTF-8');?>
+"><a href="<?php echo htmlspecialchars((string) $_smarty_tpl->tpl_vars['social_link']->value['url'], ENT_QUOTES, 'UTF-8');?>
+" target="_blank" rel="noopener noreferrer"><?php echo htmlspecialchars((string) $_smarty_tpl->tpl_vars['social_link']->value['label'], ENT_QUOTES, 'UTF-8');?>
+</a></li>
+      <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
+    </ul>
+  </div>
+<?php
+}
+}
+/* {/block 'block_social'} */
+}

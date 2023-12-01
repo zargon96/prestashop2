bf181538c617e26035da3f003068287c7d5543e6
marco <marco.crucitti96@gmail.com>
2023-12-01 10:01:51 +0100

    t

--- modules/mycustommodule/mycustommodule.php
+++ modules/mycustommodule/mycustommodule.php
@@ -10,7 +10,7 @@ class MyCustomModule extends Module
     {
         $this->name = 'mycustommodule';
         $this->tab = 'front_office_features';
-        $this->version = '1.0.1';
+        $this->version = '1.0.0';
         $this->author = 'Marco';
         $this->need_instance = 0;  
 
@@ -33,6 +33,36 @@ class MyCustomModule extends Module
 
     public function hookDisplayProductActions($params)
     {
+        // Ottenere la quantità disponibile del prodotto corrente
+        $product = $params['product'];
+        $quantityAvailable = $product->quantity;
+
+        // Determinare il testo e il colore del banner in base alla quantità disponibile
+        $bannerText = '';
+        $bannerColor = '';
+
+        if ($quantityAvailable >= 10) {
+            $bannerText = $this->l('ampiamente disponibile');
+            $bannerColor = 'green';
+        } elseif ($quantityAvailable >= 5 && $quantityAvailable < 10) {
+            $bannerText = $this->l('ultime possibilità');
+            $bannerColor = 'red';
+        } elseif ($quantityAvailable > 0 && $quantityAvailable < 5) {
+            $bannerText = $this->l('quasi terminato');
+            $bannerColor = 'red';
+        } elseif ($quantityAvailable == 0) {
+            $bannerText = $this->l('terminato');
+            $bannerColor = 'red';
+        }
+
+        // Passare i dati del banner al template
+        $this->smarty->assign(array(
+            'bannerText' => $bannerText,
+            'bannerColor' => $bannerColor,
+        ));
+
+        // Mostrare il banner nel template
         return $this->display(__FILE__, 'views/templates/hook/product_tab_content.tpl');
     }
 }
+

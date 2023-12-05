fec6fde79a504377b09c0d9afcbcc385ad9f7cd7
marco <marco.crucitti96@gmail.com>
2023-12-05 09:48:50 +0100

    t

--- modules/mycustomorderoverride/mycustomorderoverride.php
+++ modules/mycustomorderoverride/mycustomorderoverride.php
@@ -8,7 +8,7 @@ class MyCustomOrderOverride extends Module
     public function __construct()
     {
         $this->name = 'mycustomorderoverride';
-        $this->version = '1.0.0';
+        $this->version = '1.0.1';
         $this->author = 'Marco';
         $this->need_instance = 0;
 
@@ -21,7 +21,7 @@ class MyCustomOrderOverride extends Module
     public function install()
     {
         return parent::install() &&
-            $this->registerHook('actionObjectOrderAddAfter');
+            $this->registerHook('ActionGenerateDocumentReference');
     }
 
     public function uninstall()
@@ -29,6 +29,21 @@ class MyCustomOrderOverride extends Module
         return parent::uninstall();
     }
 
+    public function hookActionGenerateDocumentReference($params)
+    {
+        // Codice per la generazione della referenza dell'ordine
+        $reference = $params['reference'];
+
+        if (empty($reference)) {
+            $last_id = Db::getInstance()->getValue('SELECT MAX(id_order) FROM '._DB_PREFIX_.'orders');
+            $newReference = str_pad((int)$last_id + 1, 9, 'NR-000000', STR_PAD_LEFT);
+
+            // Aggiorna la referenza nell'oggetto $params
+            $params['reference'] = $newReference;
+        }
+
+        return $params;
+    }
     // public function hookActionObjectOrderAddAfter($params)
     // {
     
